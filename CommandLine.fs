6e581aef94795e8117a70ee05db40eec2b8ae75e
Kazuma Arino <hogeika2@gmail.com>
2022-01-30 17:53:10 +0900

    Implement findCurrentUitDir.

--- CommandLine.fs
+++ CommandLine.fs
@@ -16,3 +16,16 @@ let findTopUitDir (startDI: DirectoryInfo) =
             | _ -> findUitDir (Some nextDI) nextDI.Parent
     findUitDir None startDI
 
+/// .uitディレクトリがあるディレクトリを親に向かって探していく。
+/// 複数ある場合は一番近いモノを返す。
+/// 無ければNone
+let findCurrentUitDir (startDI: DirectoryInfo) =
+    let rec findUitDir (curDI:DirectoryInfo) =
+        match curDI with
+        | null -> None
+        | _ ->
+            match curDI.EnumerateDirectories(".uit") |> Seq.toList with
+            | [] -> findUitDir curDI.Parent
+            | _ -> (Some curDI)
+    findUitDir startDI
+
